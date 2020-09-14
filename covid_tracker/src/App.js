69baf8082c42a8c848d50b7cc3cad40a911fe927
Sanika Vanjari <sanika.vanjari@finshell.co.in>
2020-09-14 11:44:56 +0530

    added material ui library

--- covid_tracker/src/App.js
+++ covid_tracker/src/App.js
@@ -1,10 +1,21 @@
 import React from "react"
 import "./App.css"
+import { MenuItem, FormControl, Select } from "@material-ui/core"
 
 function App() {
   return (
     <div className="app">
-      <h1>Covid Tracker</h1>
+      <h1>COVID-19 TRACKER</h1>
+      <div className="app__header">
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   )
 }
