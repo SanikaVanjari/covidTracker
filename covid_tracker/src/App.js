cc6a9ef272f7fec3855be1d8d0a9e8b1ac87e74f
Sanika Vanjari <sanika.vanjari@finshell.co.in>
2020-09-14 16:08:09 +0530

    line graph complete

--- covid_tracker/src/App.js
+++ covid_tracker/src/App.js
@@ -10,6 +10,7 @@ import {
 import InfoBox from "./InfoBox"
 import Map from "./Map"
 import Table from "./Table"
+import LineGraph from "./LineGraph"
 import { sortData } from "./util"
 
 function App() {
@@ -110,6 +111,7 @@ function App() {
           <h3>Live cases by country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
+          <LineGraph />
         </CardContent>
       </Card>
     </div>
