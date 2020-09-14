c5be25e51e036665acae64267904e48ba96c10d2
Sanika Vanjari <sanika.vanjari@finshell.co.in>
2020-09-14 12:14:20 +0530

    header UI + dropdown api call

--- covid_tracker/src/App.js
+++ covid_tracker/src/App.js
@@ -1,18 +1,46 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./App.css"
 import { MenuItem, FormControl, Select } from "@material-ui/core"
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState("worldwide")
+
+  // if the useeffect input is [] -- then it will run once when the app component loads
+  useEffect(() => {
+    const getCountiresData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          //get values from the api
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }))
+          // set it to local variable
+          setCountries(countries)
+        })
+    }
+    //Call the function in useEffect
+    getCountiresData()
+  }, [])
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value
+    //Change value of the dropdown
+    setCountry(countryCode)
+  }
+
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
       <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
